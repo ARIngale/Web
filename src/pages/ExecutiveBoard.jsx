682451f9a67c354270025bcc6ca7dd6eff5689ea
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-12-10 11:10:25 +0530

    updated alignment of cards

--- src/pages/ExecutiveBoard.jsx
+++ src/pages/ExecutiveBoard.jsx
@@ -3,33 +3,52 @@ import TeamsNav from '../components/TeamNav';
 import Board from '../components/Card'
 import Background from '../components/Background';
 import cardData from '../Data/ExecutiveBoardMembers'
+import { h1 } from 'framer-motion/client';
 
 function ExecutiveBoard() {
   
+  const groupedByPosition = cardData.reduce((acc, data) => {
+    data.members.forEach(member => {
+      const memberWithPosition = { ...member, position: data.position };
+      if (!acc[data.position]) {
+        acc[data.position] = [];
+      }
+      acc[data.position].push(memberWithPosition);
+    });
+    return acc;
+  }, {});
 
   return (
     <div>
-        <Background />
-        <div className="container flex justify-center pt-32 pb-10 w-screen"><TeamsNav /></div>
-        <h1 className='text-center text-white text-6xl font-bold'>Executive Board</h1>
-        <div className="container flex gap-14 w-screen justify-center p-10 flex-wrap">
-            
-        {cardData.map((data, index) => (
-        data.members.map((member, memberIndex) => (
-          <Board
-            key={`${index}-${memberIndex}`}
-            name={member.name}
-            post={data.position}
-            imageUrl={member.profilePic}
-            socialLinks={member.socialHandles}
-          />
-        ))
-      ))}
-      
-    </div>
-      
+      <Background />
+      <div className="container flex justify-center pt-32 pb-2 w-screen">
+        <TeamsNav />
+      </div>
+      <h1 className='text-center text-white text-5xl font-bold'>Executive Board</h1>
+      <div className="container flex flex-col gap-10 w-screen justify-center p-4">
+        {Object.keys(groupedByPosition).map((position, posIndex) => (
+          <div key={posIndex} className="flex flex-wrap justify-center gap-3 w-full">
+            <h1 className='w-screen text-center text-white text-2xl'>{position}</h1>
+            {groupedByPosition[position].map((member, memberIndex) => (
+              
+              <div key={memberIndex} className="flex justify-center w-full sm:w-1/2 lg:w-1/4 mb-10">
+                
+                <Board
+                  name={member.name}
+                  post={member.position}  
+                  imageUrl={member.profilePic}
+                  socialLinks={member.socialHandles}
+                  showQuote={false}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
+
+
 };
 
 
--- src/pages/MainBoard.jsx
+++ src/pages/MainBoard.jsx
@@ -5,30 +5,43 @@ import Board from '../components/Card'
 import cardData from '../Data/MainBoardMembers'
 
 function MainBoard() {
+  const groupedByPosition = cardData.reduce((acc, data) => {
+    data.members.forEach(member => {
+      const memberWithPosition = { ...member, position: data.position };
+      if (!acc[data.position]) {
+        acc[data.position] = [];
+      }
+      acc[data.position].push(memberWithPosition);
+    });
+    return acc;
+  }, {});
 
   return (
     <div>
-        <Background />
-        <div className="container flex justify-center pt-32 pb-10 w-screen"><TeamsNav /></div>
-        
-    
-        <h1 className='text-center text-white text-6xl font-bold'>Main Board</h1>
-        <div className="container flex gap-14 w-screen justify-center p-10 flex-wrap">
-            
-        {cardData.map((data, index) => (
-        data.members.map((member, memberIndex) => (
-          <Board
-            key={`${index}-${memberIndex}`}
-            name={member.name}
-            post={data.position}
-            imageUrl={member.profilePic}
-            socialLinks={member.socialHandles}
-          />
-        ))
-      ))}
-      
-    </div>
-      
+      <Background />
+      <div className="container flex justify-center pt-32 pb-2 w-screen">
+        <TeamsNav />
+      </div>
+      <h1 className='text-center text-white text-5xl font-bold'>Main Board</h1>
+      <div className="container flex flex-col gap-14 w-screen items-center justify-center p-4">
+        {Object.keys(groupedByPosition).map((position, posIndex) => (
+          <div key={posIndex} className="flex flex-wrap justify-center gap-3 w-full">
+            <h1 className='w-screen text-center text-white text-2xl'>{position}</h1>
+            {groupedByPosition[position].map((member, memberIndex) => (
+              <div key={memberIndex} className="flex items-center justify-center w-full sm:w-1/2 lg:w-1/3 mb-10">
+                <Board
+                  name={member.name}
+                  post={member.position}  
+                  imageUrl={member.profilePic}
+                  socialLinks={member.socialHandles}
+                  quote={member.quote}
+                  showQuote={true}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
